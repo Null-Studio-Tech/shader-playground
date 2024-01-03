c325bdcd5251c71b22c7b40e3d065fc7a55bd6b1
dongzhigang <dongzhigang@megvii.com>
2024-01-03 22:58:47 +0800

    feat: 更新流程

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import SkyVertexShader from './shaders/sky.vert';
 import SkyFragmentShader from './shaders/sky.frag';
 // import UpVertexShader from './shaders/up.vert';
 // import UpFragmentShader from './shaders/up.frag';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
+// import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import { randomGaussian } from './utils/gaussian.utils';
 
 const renderConfig = {
@@ -19,12 +19,6 @@ const createRender = () => {
   const render = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   render.setPixelRatio(window.devicePixelRatio);
   render.setSize(renderConfig.RENDER_WIDTH, renderConfig.RENDER_HEIGHT);
-
-  window.addEventListener('resize', () => {
-    console.log('render size', renderConfig.RENDER_WIDTH, renderConfig.RENDER_HEIGHT)
-    render.setSize(renderConfig.RENDER_WIDTH, renderConfig.RENDER_HEIGHT)
-    render.setPixelRatio(window.devicePixelRatio);
-  });
   return render;
 }
 
@@ -35,8 +29,10 @@ const createScene = () => {
 }
 
 const createCamera = (camera?: THREE.PerspectiveCamera) => {
-  const fov = THREE.MathUtils.radToDeg(2 * Math.atan(36 / 160));
-  const _camera = camera ? camera : new THREE.PerspectiveCamera(fov, renderConfig.RENDER_ASPECT, 0.1, 2000);
+  const houdiniFocalLength = 80;
+  const houdiniAperture = 41.4214;
+  const fov = 2 * Math.atan(houdiniAperture / (2 * houdiniFocalLength)) * (180 / Math.PI);
+  const _camera = camera ? camera : new THREE.PerspectiveCamera(fov, renderConfig.RENDER_ASPECT, 0.1, 1000);
   return _camera;
 }
 
@@ -80,7 +76,8 @@ const createGroundParticle: () => [THREE.Points, THREE.ShaderMaterial] = () => {
   const particleMaterial = new THREE.ShaderMaterial({
     uniforms: {
       'uTotal': { value: total },
-      'uTime': { value: 0 }
+      'uTime': { value: 0 },
+      'uMouse': { value: new THREE.Vector3(100, 100, 100) },
     },
     vertexShader: VertexShader,
     fragmentShader: FragmentShader,
@@ -194,36 +191,63 @@ const init = () => {
   const [upParticles, upMaterial] = createUpParticle();
   scene.add(upParticles);
   upParticles.position.set(0, 0, 0);
-  const [outerSkyParticles, outerSkyMaterial] = createSkyParticle(0.5);
-  scene.add(outerSkyParticles);
-  outerSkyParticles.position.set(0.0, 1.2, 0.0);
-  const [innerSkyParticles, innerSkyMaterial] = createSkyParticle(0.4);
-  scene.add(innerSkyParticles);
-  innerSkyParticles.position.set(0.0, 1.2, 0.0);
-  camera.position.setX(0);
-  camera.position.setZ(5.5);
-  camera.position.setY(1.1);
-  camera.lookAt(0, 1.1, 0);
-
+  //const [outerSkyParticles, outerSkyMaterial] = createSkyParticle(0.5);
+  //scene.add(outerSkyParticles);
+  //outerSkyParticles.position.set(0, 0, 0.5);
+  //const [innerSkyParticles, innerSkyMaterial] = createSkyParticle(0.4);
+  //scene.add(innerSkyParticles);
+  //innerSkyParticles.position.set(0, 0, 0.5);
+  camera.position.set(0, 1.1, 5.5);
+  //camera.lookAt(0, 1.1, 0);
 
   window.addEventListener("resize", () => {
+    console.log('render size', renderConfig.RENDER_WIDTH, renderConfig.RENDER_HEIGHT)
+    renderer.setSize(renderConfig.RENDER_WIDTH, renderConfig.RENDER_HEIGHT)
+    renderer.setPixelRatio(window.devicePixelRatio);
     camera.aspect = renderConfig.RENDER_ASPECT;
     camera.updateProjectionMatrix();
-    renderer.setSize(renderConfig.RENDER_WIDTH, renderConfig.RENDER_HEIGHT);
+  })
+  //const control = new OrbitControls(camera, renderer.domElement);
+  //control.addEventListener('change', () => {
+  //  renderer.render(scene, camera)
+  //})
+  const { x, y, width, height } = renderer.domElement.getBoundingClientRect();
+  const raycaster = new THREE.Raycaster();
+  const pointer = new THREE.Vector2();
+  const planeGeo = new THREE.Plane();
+  planeGeo.setFromNormalAndCoplanarPoint(camera.getWorldDirection(planeGeo.normal), groundParticles.position);
+  const virPlane = new THREE.Mesh(planeGeo,new THREE.MeshBasicMaterial());
+  const ball = new THREE.Mesh(new THREE.SphereGeometry(0.001), new THREE.PointsMaterial({ color: 0xff0000 }));
+  scene.add(ball);
+
+  renderer.domElement.addEventListener('pointermove', (e) => {
+    pointer.x = ((e.clientX + x) / width) * 2 - 1;
+    pointer.y = - ((e.clientY + y) / height) * 2 + 1;
+
+    raycaster.setFromCamera(pointer, camera);
+    const intersects = raycaster.intersectObjects([groundParticles], false);
+    if (intersects.length > 0) {
+      groundMaterial.uniforms.uMouse.value = intersects[0].point;
+      virPlane.setFromNormalAndCoplanarPoint(camera.getWorldDirection(virPlane.normal), intersects[0].object.position);
+      const [x, y, z] = intersects[0].point;
+      console.log(x,y,z)
+      ball.position.set(x, y, z);
+    }
+    // intersects.forEach(mesh => {
+    //   console.log(mesh.point);
+    // })
   })
 
-  const control = new OrbitControls(camera, renderer.domElement);
-  control.addEventListener('change', () => {
-    renderer.render(scene, camera)
-  })
+
+
 
   renderer.render(scene, camera);
 
   const animate = () => {
     const delta = clock.getDelta()
     groundMaterial.uniforms.uTime.value += delta;
-    outerSkyMaterial.uniforms.uTime.value += delta;
-    innerSkyMaterial.uniforms.uTime.value += delta;
+    //outerSkyMaterial.uniforms.uTime.value += delta;
+    //innerSkyMaterial.uniforms.uTime.value += delta;
     upMaterial.uniforms.uTime.value += delta;
     renderer.render(scene, camera);
     requestAnimationFrame(animate)
